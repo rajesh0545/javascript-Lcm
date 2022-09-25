b78b9f4af098d24df2f7d76859bba365467c838b
rajesh <krajeshgowri7032@mail.com>
2022-09-25 18:51:07 +0530

    add files

---
+++ lcm.js
@@ -0,0 +1,33 @@
+// const num1=5;
+// const num2=6;
+// let min = (num1>num2)? num1:num2;
+
+// while(true){
+//     if(min%num1==0 && min % num2==0){
+//         console.log(`the lcm of ${num1} and ${num2} is ${min}`)
+//         break;
+//     }
+//     min++;
+// }
+
+
+// function LCM(num1,num2){
+//     for(i=1;i<(num1*num2);i++){
+//         if(i%num1==0 && i%num2==0 ){
+           
+//         }
+//     }
+//     console.log(`the LCM of ${num1},${num2} is ${i}`)
+// }
+// LCM(2,3)
+
+function Lcm(num1,num2){
+    for(i=1;i<(num1*num2);i++){
+        if(i%num1==0 && i%num2==0){
+
+        }
+    }
+    console.log(` Lcm of${num1} and ${num2}  is a ${i}`)
+
+}
+Lcm(20,30);
